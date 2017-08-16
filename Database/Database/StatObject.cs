f775cbe1346e9d63bc9ee0ff57d73bde5e1126b8
RockmanZheng <rockman-zheng@qq.com>
2017-08-16 17:21:40 +0800

    初步加入 XML 数据库脚本

---
+++ Database/Database/StatObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    /// <summary>
+    /// 对应 stat 表单一个记录
+    /// </summary>
+    /// 
+    [Serializable]
+    public class StatObject : DataObject
+    {
+        public int score
+        {
+            get
+            {
+                return (int)Get("score");
+            }
+            set
+            {
+                Set("score", value);
+            }
+        }
+
+        public StatObject()
+        {
+            variables.Add(new NamedVariable("score", "int"));
+        }
+
+        public StatObject(DataObject dataObj)
+        {
+            variables = dataObj.variables;
+        }
+    }
+}
